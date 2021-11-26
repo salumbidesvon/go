b0a06274bffa63f3ead83d365ed711fb4f36a7b0
vjsalumbides <elmjethro@gmail.com>
2021-11-26 20:18:50 +0800

    added test

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -6,6 +6,6 @@ func TestNewDeck(t *testing.T) {
 	d := newDeck()
 
 	if len(d) != 16 {
-		t.Errorf("Expected deck lengt of 20, but got", len(d))
+		t.Errorf("Expected deck lengt of 16, but got %v", len(d))
 	}
 }
